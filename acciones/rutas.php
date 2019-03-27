34b2eca794cc47ffe32c40019ddcdd615d93b56a
felipe <pipe9906@gmail.com>
2019-03-27 12:16:20 -0500

    q

--- acciones/rutas.php
+++ acciones/rutas.php
@@ -18,16 +18,16 @@
 		$tibu_ruta= ($_POST['tibu_ruta']);
 
 	
-		$sentencia="INSERT INTO rutas VALUES ('', '$nom_ruta', '$desc_ruta', '$fue_ruta', '$dest_ruta', '$tibu_ruta')";
+		$sentencia="insert into rutas values ('', '$nom_ruta', '$desc_ruta', '$fue_ruta', '$dest_ruta', '$tibu_ruta')";
 		mysqli_query($con,$sentencia);
 
 
 
-echo $nom_ruta;
-echo $desc_ruta;
-echo $fue_ruta;
-echo $dest_ruta;
-echo $tibu_ruta;
+
 
 ?>
 
+<script type="text/javascript">
+	alert("Ruta registrada");
+	window.location.href="../index.php";
+</script>
